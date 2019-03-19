385dc6eec755aba8b1bd1cdc4ffaff48ffd282d3
josedlca <jose.dlcruz.ayala@gmail.com>
2019-03-19 18:27:39 -0500

    our client end

--- src/pages/home/ourclient/Ourclient.jsx
+++ src/pages/home/ourclient/Ourclient.jsx
@@ -9,6 +9,58 @@ class Ourclient extends Component {
                 <div className="container">
                     <Headerspage title={'Our Client'} description={'Our trusted business partner'} />
                     <div className="row">
+                        <div className="col-md-12">
+                            <div className="ourclient__up">
+                                <div className="row justify-content-around">
+                                    <div className="col-md-2">
+                                        <div className="ourclient__brand">
+                                        </div>
+                                    </div>
+                                    <div className="col-md-2">
+                                        <div className="ourclient__brand">
+                                        </div>
+                                    </div>
+                                    <div className="col-md-2">
+                                        <div className="ourclient__brand">
+                                        </div>
+                                    </div>
+                                    <div className="col-md-2">
+                                        <div className="ourclient__brand">
+                                        </div>
+                                    </div>
+                                    <div className="col-md-2">
+                                        <div className="ourclient__brand">
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-md-12">
+                            <div className="ourclient__bot">
+                                <div className="row justify-content-around">
+                                    <div className="col-md-2">
+                                        <div className="ourclient__brand">
+                                        </div>
+                                    </div>
+                                    <div className="col-md-2">
+                                        <div className="ourclient__brand">
+                                        </div>
+                                    </div>
+                                    <div className="col-md-2">
+                                        <div className="ourclient__brand">
+                                        </div>
+                                    </div>
+                                    <div className="col-md-2">
+                                        <div className="ourclient__brand">
+                                        </div>
+                                    </div>
+                                    <div className="col-md-2">
+                                        <div className="ourclient__brand">
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </section>    
